0c06b89be157f59770bb40c60bd205e89cfcde37
bazsa124 <40303378+bazsa124@users.noreply.github.com>
2024-05-01 15:24:37 +0200

    Update Rules
    Completed Rules cards, rules script file with empty functions

---
+++ KoSoftBoardGame/src/main/resources/static/rules.js
@@ -0,0 +1,71 @@
+//A
+
+function checkForestEdge(){
+
+}
+
+function checkForestRowColumn(){
+
+}
+
+function checkForestSurrounded(){
+
+}
+
+function checkForestMountainBridge(){
+
+}
+
+//B
+
+function checkWaterByField(){
+
+}
+
+function checkWaterFieldToRuin(){
+
+}
+
+function checkWaterFieldNotNext(){
+
+}
+
+function checkWaterFieldToMountain(){
+
+}
+
+//C
+
+function checkHouseBiggerThan(){
+
+}
+
+function checkHouseBiggestNotMountain(){
+
+}
+
+function checkHouseNextToDifferents(){
+
+}
+
+function checkHouseSecondBiggest(){
+
+}
+
+//D
+
+function checkComplitedRowColumn(){
+
+}
+
+function checkBiggestFilledSquare(){
+
+}
+
+function checkCompetedDiagonal(){
+
+}
+
+function checkSurrendedEmpty(){
+
+}
